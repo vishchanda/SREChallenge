7e50d8ec874b42bf33fe04d541e7502283b08ca8
vishchanda <37004564+vishchanda@users.noreply.github.com>
2021-03-07 12:18:46 -0500

    Create merge-the-tools.py

---
+++ merge-the-tools.py
@@ -0,0 +1,14 @@
+def merge_the_tools(string, k):
+    # your code goes here
+    if ((1<=len(string)<=(10**4)) and (1<=k<=len(string)) and ((len(string)%k)==0)):
+      u = ''
+      for i in range(len(string)):
+          if string[i] not in u:
+              u+=string[i]
+          if (i >= 0) and ((i+1)%k == 0):
+            print(u)
+            u = ''
+    else:  print("Constraints voilated !!")     
+if __name__ == '__main__':
+    string, k = input(), int(input())
+    merge_the_tools(string, k)
